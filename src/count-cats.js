bf551a4d6a40ed395dfa030392bc55175260efd1
vustal <vita.ustalkova@bk.ru>
2021-11-04 14:18:01 +0300

    feat: desition count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function countCats(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === '^^') count++;
+    }  
+  }
+  return count;
 }
+
