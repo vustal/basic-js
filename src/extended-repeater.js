ddc4e51fae616f869676ade44c02902f21fce35f
vustal <vita.ustalkova@bk.ru>
2021-11-12 00:09:12 +0300

    feat: add a solution extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  //throw new NotImplementedError('Not implemented');
+  let array = [];
+  let i = 0;
+  do {
+    array.push(String(str));
+    i++;
+  } while (i < options.repeatTimes);
+
+  let additions = [];
+  if ('addition' in options) {
+    let j = 0;
+    do {
+      additions.push(String(options.addition));
+      j++;
+    } while (j < options.additionRepeatTimes);
+  }
+  
+  const add = 'additionSeparator' in options ? 
+    additions.join(options.additionSeparator) : 
+    additions.join('|');
+
+  const new_array = array.map( item => item + add);
+ 
+  return 'separator' in options ? 
+    new_array.join(options.separator) : 
+    new_array.join('+');
 }
